be79ce00cdd7c3c622b796ecf5eaa00735ac2355
Pablo Daniel Sívori <sivori.daniel@gmail.com>
2017-09-10 17:52:41 -0300

    Se cambia la ruta de la base por la de mongolab

--- src/model/db.js
+++ src/model/db.js
@@ -1,5 +1,5 @@
 exports = mongoose = require('mongoose');
-var connectionUri = "mongodb://127.0.0.1/dbtp0";
-//var connectionUri = "mongodb://mongo:mongo@ds131854.mlab.com:31854/linkup";
+//var connectionUri = "mongodb://127.0.0.1/dbtp0";
+var connectionUri = "mongodb://mongo:mongo@ds131854.mlab.com:31854/linkup";
 mongoose.connect(connectionUri,{ useMongoClient: true });
 exports = Schema = mongoose.Schema;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-//Module dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var url = require('url');
